50d3f0e0d513e02abdc1d3c72a0a4851dafa7a1f
Pwkajonwit <powertec.civil@gmail.com>
2025-08-09 15:58:25 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,6 @@
+      
+       const WEB_APP_URL = 'https://script.google.com/macros/s/XXXXXXXX/exec';
+       const CONFIG_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+
+       const LIFF_ID = 'XXXX-XXXXX'; // Booking
+       const LIFF_ID_HIS = 'XXXX-XXXXX'; // History
