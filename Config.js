2a23fb6259535d3354552e254f27390e0193c9f0
Pwkajonwit <powertec.civil@gmail.com>
2025-08-09 16:15:41 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
       
-       const WEB_APP_URL = 'https://script.google.com/macros/s/XXXXXXXX/exec';
-       const CONFIG_URL = 'https://script.google.com/macros/s/XXXXXXXXX/exec';
+       const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw2FAbKPmtcnVLz-U-YvoqgLtyjh_CbX2jLcPyU9nJYzGUodtDv5RBopcVi6VKr41yMPA/exec';
+       const CONFIG_URL = 'https://script.google.com/macros/s/AKfycbwaogftjZzA1XT5csbg-loxzrMy-Tmq-_VEh5Wqh5g1_kwvwvqEomhlnUFSq-qAHvOICg/exec';
 
-       const LIFF_ID = 'XXXX-XXXXX'; // Booking
-       const LIFF_ID_HIS = 'XXXX-XXXXX'; // History
+       const LIFF_ID = '2007900240-r50Oo75m'; // Booking
+       const LIFF_ID_HIS = '2007900240-PXwpMjYm'; // History
